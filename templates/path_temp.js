c1c7ff54bf3511db54d3df61a71122a91bb64eda
adussaq <a.m.dussaq@gmail.com>
2016-07-11 10:52:47 -0500

    Remove duplicated sentence

--- templates/path_temp.js
+++ templates/path_temp.js
@@ -8,11 +8,4 @@ sentence({
         states: [off, 'Male', 'Female (Not Pregnant)', 'Female (Pregnant)']
     })
 });
-sentence({
-    format: '{x} patient is ... ',
-    x: ordinal({
-        long_name: '',
-        short_name: 'Gender',
-        states: [off, 'Male', 'Female (Not Pregnant)', 'Female (Pregnant)']
-    })
-});
+
