8aa562f83489b80011251fbf12b4570ec0322101
adussaq <a.m.dussaq@gmail.com>
2016-07-11 10:51:55 -0500

    repeat first sentence for testing

--- templates/path_temp.js
+++ templates/path_temp.js
@@ -8,3 +8,11 @@ sentence({
         states: [off, 'Male', 'Female (Not Pregnant)', 'Female (Pregnant)']
     })
 });
+sentence({
+    format: '{x} patient is ... ',
+    x: ordinal({
+        long_name: '',
+        short_name: 'Gender',
+        states: [off, 'Male', 'Female (Not Pregnant)', 'Female (Pregnant)']
+    })
+});
