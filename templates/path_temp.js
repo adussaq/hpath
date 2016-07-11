d7f961374d64c1c6d32df2bbc36b0d79bdf95964
adussaq <a.m.dussaq@gmail.com>
2016-07-11 10:50:19 -0500

    Create path_temp.js
    Just a simple sentence to start with

---
+++ templates/path_temp.js
@@ -0,0 +1,10 @@
+section('Patient Information');
+
+sentence({
+    format: '{x} patient is ... ',
+    x: ordinal({
+        long_name: '',
+        short_name: 'Gender',
+        states: [off, 'Male', 'Female (Not Pregnant)', 'Female (Pregnant)']
+    })
+});
